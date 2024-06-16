6a2c17455e799290dadf72151566c872c3764fae
jospin6 <jospinndagano1@gmail.com>
2024-06-16 20:57:10 +0200

    Add published_time function to post module

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,14 +3,25 @@ module PostsHelper
         tags = tags_string.split(',').map(&:strip)
         tags.map { |tag| "##{tag}" }.join(' ')
     end
-    
-    def truncate_text(text, length = 20)
-        if text.length > length
-            "#{text[0, length]}..."
+
+    def published_time(timestamp)
+        now = Time.now
+        diff = now - timestamp.to_time
+
+        if diff < 60
+            "posted instantanely"
+
+        elsif diff < 3600
+            "#{(diff / 60).to_i} minutes ago"
+        elsif diff < 86400
+            "#{(diff / 3600).to_i} hours ago"
+        elsif diff < 604800
+            "#{(diff / 86400).to_i} days ago"
         else
-            text
+            timestamp.strftime("%d %b %Y")
         end
         
+
     end
     
 
