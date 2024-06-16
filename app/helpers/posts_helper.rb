8c6f0231af773b4b3bdf1b9d771adcfec6dcd967
jospin6 <jospinndagano1@gmail.com>
2024-06-16 18:34:17 +0200

    Add display_tags to posts helper to desplay tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+    def display_tags(tags_string)
+        tags = tags_string.split(',').map(&:strip)
+        tags.map { |tag| "##{tag}" }.join(' ')
+    end
+    
+    def truncate_text(text, length = 20)
+        if text.length > length
+            "#{text[0, length]}..."
+        else
+            text
+        end
+        
+    end
+    
+
 end
