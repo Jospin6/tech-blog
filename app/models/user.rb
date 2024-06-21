e9d1db6e2ddc519e209efa37d98745ed18d66374
jospin6 <jospinndagano1@gmail.com>
2024-06-21 12:39:11 +0200

    Add validation rules to the username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, and :omniauthable
+  validates :username, 
+            presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: {maximum: 25}, 
+            format: { with: /\A[a-zA-Z0-9 _\.]*\z/ }
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
 end
