91e38e28d88d3196ed10668bd7373b521adf2cf0
jospin6 <jospinndagano1@gmail.com>
2024-06-21 12:17:42 +0200

    Add image to the user migration

--- db/migrate/20240621094150_add_usernname_to_users.rb
+++ db/migrate/20240621094150_add_usernname_to_users.rb
@@ -3,6 +3,7 @@ class AddUsernnameToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :username, :string
     add_column :users, :skills, :string
     add_column :users, :bio, :text
+    add_column :users, :image, :text
     add_index :users, :username, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_21_094150) do
     t.string "username"
     t.string "skills"
     t.text "bio"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
