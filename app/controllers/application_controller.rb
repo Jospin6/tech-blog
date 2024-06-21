ae8584b5688d35ce8f18d8d6b69c68ce4c1be5fd
jospin6 <jospinndagano1@gmail.com>
2024-06-21 12:31:59 +0200

    Add a function to configure devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     include PostsHelper
+    before_action :configure_devise_params, if: :devise_controller?
+        
+    def configure_devise_params
+        devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    end
+    
+    
 end
