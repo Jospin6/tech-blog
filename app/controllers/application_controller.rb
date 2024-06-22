6959bdfedde56bb3c3cc77ea6f0cbbfbfcbb59ae
jospin6 <jospinndagano1@gmail.com>
2024-06-22 08:48:05 +0200

    Design user edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_devise_params, if: :devise_controller?
         
     def configure_devise_params
-        devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+        devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :email, :password, :password_confirmation, :skills, :bio, :image) }
     end
     
     
