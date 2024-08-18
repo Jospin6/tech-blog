b781ded32a5f1d5833a930a3ad835c850542d47d
jospin6 <jospinndagano1@gmail.com>
2024-08-18 18:31:42 +0200

    Fix the bug related to the user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_devise_params, if: :devise_controller?
         
     def configure_devise_params
-        devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :email, :password, :password_confirmation, :skills, :bio, :image) }
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :skills, :bio, :image) }
         devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :skills, :bio, :image, :current_password) }
     end
     
