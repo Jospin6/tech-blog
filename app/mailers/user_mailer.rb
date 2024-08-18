5451504b966783748183c2cd4bd4a779f4a7883d
jospin6 <jospinndagano1@gmail.com>
2024-08-18 21:33:22 +0200

    Configure postmark and devise for user account confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+    def confirmation_instructions(user, token, opts = {})
+        @user = user
+        @token = token
+        mail(to: @user.email, subject: 'Your account confirmation')
+    end
 end
