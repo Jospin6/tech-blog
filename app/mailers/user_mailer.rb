0a3b0d7ce01f2a5e7a7654ab53b764913f7fd72e
jospin6 <jospinndagano1@gmail.com>
2024-08-19 08:56:34 +0200

    Add from to the mail function

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
     def confirmation_instructions(user, token, opts = {})
         @user = user
         @token = token
-        mail(to: @user.email, subject: 'Your account confirmation')
+        mail(from: 'jospinndagano1@gmail.com',to: @user.email, subject: 'Your account confirmation')
     end
 end
