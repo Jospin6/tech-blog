5d7ca29d7fd2aefa157d26a0cf00bd74620280ac
jospin6 <jospinndagano1@gmail.com>
2024-08-25 02:13:00 +0200

    Apply migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_22_161549) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_22_091258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,10 +83,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_22_161549) do
     t.text "bio"
     t.text "image"
     t.boolean "is_admin", default: false
-    t.string "unconfirmed_email"
-    t.datetime "confirmation_sent_at"
-    t.datetime "confirmed_at"
-    t.string "confirmation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
