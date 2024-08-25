ca367eaa208ecdfc414fd769fd9d1cf1a3123d24
jospin6 <jospinndagano1@gmail.com>
2024-08-25 02:41:08 +0200

    Modify cancancan ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
     # Define abilities for the user here. For example:
     user ||= User.new
     
-    if user.id == User.first.id
+    if user.id == User.first&.id
       can :manage, :all
     end
     
